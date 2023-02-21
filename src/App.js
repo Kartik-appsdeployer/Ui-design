709cc5ad414e1c4050478206e77ad4e36e5cd1aa
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2023-02-21 16:37:24 +0530

    Added the Pagination and Width expantion using Material UI

--- src/App.js
+++ src/App.js
@@ -1,63 +1,19 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import BootstrapTable from 'react-bootstrap-table-next';
-import paginationFactory from 'react-bootstrap-table2-paginator';
-import Data from './data.json'
-import axios from 'axios';
-import Loader from 'lodash';
+import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+import Box from '@mui/material/Box';
+import { DataGridPro } from '@mui/x-data-grid-pro';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import './App.css'
 
-const pageSize = 200;
-function App() {
-  const [State, setState] = useState(Data)
-  const [data, setData] = useState("");
-  const [currentPage, setCurrentPage] = useState(1)
-  const [paginatedPosts, setPaginatedPosts] = useState()
-  // const columns = [
-  //   { dataField: 'id', text: 'ID' },
-  //   { dataField: 'title', text: 'title' },
-  //   { dataField: 'description', text: 'description' },
-  //   { dataField: 'location', text: 'location' },
-  //   { dataField: 'lng', text: 'lng' },
-  //   { dataField: 'lat', text: 'lat' },
-  //   { dataField: 'userId', text: 'userId' },
-  //   { dataField: 'name', text: 'name' },
-  //   { dataField: 'isdeleted', text: 'isdeleted' },
-  //   { dataField: 'videoUrl', text: 'videoUrl' },
-  //   { dataField: 'images', text: 'images' },
-  //   { dataField: 'mediatype', text: 'mediatype' },
-  //   { dataField: 'imagePaths', text: 'imagePaths' },
-  //   { dataField: 'feedsComment', text: 'feedsComment' },
-  //   { dataField: 'commentCount', text: 'commentCount' },
-  //   { dataField: 'createdAt', text: 'createdAt' },
-  //   { dataField: 'code', text: 'code' },
-  //   { dataField: 'msg', text: 'msg' },
-  // ];
-  // // console.log(data, "Dataaaaa");
-  // const paginationOptions = {
-  //   sizePerPage: 100,  // number of rows to show per page
-  //   hideSizePerPage: true,  // hide the option to change number of rows per page
-  //   hidePageListOnlyOnePage: true,  // hide pagination controls when there is only one page
-  // };
-  
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
-      setState(res.data);
-      setPaginatedPosts(Loader(State).slice(0).take(pageSize).value());
-    })
-  }, [])
-  // 
-  const pageCount = State ? Math.ceil(State.length / pageSize) : 0;
-  if (pageCount === 1) return null;
-  const pages = Loader.range(1, pageCount + 1);
-  const pagination = (page) => {
-    setCurrentPage(page);
-    const startIndex = (page - 1) * pageSize;
-    const paginatePost = Loader(State).slice(startIndex).take(pageSize).value();
-    setPaginatedPosts(paginatePost);
-  } 
+export default function SimpleTable() {
+  const [pageSize, setPageSize] = React.useState(100);
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 10000,
+    editable: true,
+  });
   return (
-    <div className="App">
+    <div>
       <div className="navBar">
 
       </div>
@@ -69,59 +25,25 @@ function App() {
         </div>
 
         <div style={{ minWidth: '85%' }}>
-          <p>Get the result here: <span style={{ fontWeight: "bold" }}>{data}</span></p>
           <div className="my-Table">
-            {/* <BootstrapTable
-              keyField="id"
-              data={State}
-              columns={columns}
-              pagination={paginationFactory(paginationOptions)}
-              
-            /> */}
-            <Table responsive bordered>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Post ID</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Body</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  paginatedPosts.map((obj, i) => (
-                    <tr key={i}>
-                      <td onClick={() => setData(obj.id)}>{obj.id}</td>
-                      <td onClick={() => setData(obj.albumId)}>{obj.albumId}</td>
-                      <td onClick={() => setData(obj.title)}>{obj.title}</td>
-                      <td onClick={() => setData(obj.url)}>{obj.url}</td>
-                      <td onClick={() => setData(obj.thumbnailUrl)}>{obj.thumbnailUrl}</td>
-                      </tr>
-                  ))
-                }
-              </tbody>
-            </Table>
+            <Box sx={{ height: 520, width: '100%' }}>
+              <DataGridPro
+                {...data}
+                loading={data.rows.length === 0}
+                rowHeight={38}
+                checkboxSelection
+                disableSelectionOnClick
+                experimentalFeatures={{ newEditingApi: true }}
+                pageSize={pageSize}
+                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                rowsPerPageOptions={[5, 10, 20]}
+                pagination
+                {...data}
+              />
+            </Box>
           </div>
-          <nav className='d-flex justify-content-center'>
-            <ul className="pagination">
-              {
-                pages.map((page) => (
-                  <li className={
-                    page === currentPage ? "page-item active" : "page-item"
-                  } style={{cursor: "pointer"}}>
-                    <p className="page-link"
-                    onClick={() => pagination(page)}>{page}</p>
-                  </li>
-                ))
-              }
-            </ul>
-          </nav>
         </div>
       </div>
-
     </div>
   );
 }
-
-export default App;
